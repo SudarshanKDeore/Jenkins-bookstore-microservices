c08de09fcfae5e3eea0bb7c5cb2d5e3ff13b38d6
Sudarshan Deore <122254130+SudarshanKDeore@users.noreply.github.com>
2025-12-31 16:36:39 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -7,11 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
-    <h2>Book Store</h2>
+    <h2>Bookstore App</h2>
     <form method="POST" action="/books">
       <input name="name" placeholder="Book Name"/>
       <button>Add Book</button>
     </form>
+
     <form method="POST" action="/authors">
       <input name="name" placeholder="Author Name"/>
       <button>Add Author</button>
@@ -20,13 +21,19 @@ app.get("/", (req, res) => {
 });
 
 app.post("/books", async (req, res) => {
-  await axios.post("http://book-service:8080/books", { name: req.body.name });
+  await axios.post("http://book-service:8080/books", {
+    name: req.body.name
+  });
   res.redirect("/");
 });
 
 app.post("/authors", async (req, res) => {
-  await axios.post("http://author-service:8080/authors", { name: req.body.name });
+  await axios.post("http://author-service:8080/authors", {
+    name: req.body.name
+  });
   res.redirect("/");
 });
 
-app.listen(3000, () => console.log("Frontend running on 3000"));
+app.listen(3000, () => {
+  console.log("Frontend running on port 3000");
+});
