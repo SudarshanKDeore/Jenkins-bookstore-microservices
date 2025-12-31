951320f9b37abe97fb8a6de2b0c4d0599028428f
Sudarshan Deore <122254130+SudarshanKDeore@users.noreply.github.com>
2025-12-31 16:25:18 +0530

    Delete book-service/Book.java

--- book-service/Book.java
+++
@@ -1,8 +0,0 @@
-@Entity
-public class Book {
-  @Id @GeneratedValue
-  private Long id;
-  private String name;
-  public String getName() { return name; }
-  public void setName(String name) { this.name = name; }
-}
